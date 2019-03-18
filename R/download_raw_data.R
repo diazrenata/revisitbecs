c34d67b04ba7f0a95fc193e7489e7c33a102d76b
Renata Diaz <diaz.renatam@gmail.com>
2019-03-18 11:30:00 -0400

    Set up as research compendium, create simulation functions.

---
+++ R/download_raw_data.R
@@ -0,0 +1,14 @@
+#' @title Download raw paper data
+#'
+#' @description Download datasets used in Ernest 2005.
+#'
+#' @param datapath Folder to store the data
+#'
+#' @return NULL
+#'
+#' @export
+
+download_raw_paper_data <- function(datapath = here::here('data', 'paper', 'raw'))
+{
+
+}
