a230fee53a9da4698f0f1cdc6b34ecd537a9b5ca
Renata Diaz <diaz.renatam@gmail.com>
2019-05-03 10:21:05 -0400

    function to process Niwot data

--- R/ernest_2005_data.R
+++ R/ernest_2005_data.R
@@ -72,103 +72,25 @@ process_andrews_data <- function() {
 #'
 #' @return NULL
 #'
-process_niwot_data <- function(datapath = here::here('data', 'paper', 'raw', 'niwot')){
-  # FROM LTER WEBSITE #
+process_niwot_data <- function(datapath = here::here()){
+  # Halfpenny, Jim. 2019. Small mammal disturbance data for Niwot Ridge from 1981-6-30 to 1990-8-23, yearly. http://niwot.colorado.edu 
+  # http://niwot.colorado.edu/data/data/small-mammal-species-composition-data-for-niwot-ridge-1981-1990
   
-  #Package ID: knb-lter-nwt.43.3 Cataloging System:https://pasta.lternet.edu.
-  # Data set title: Small mammal herbivore trapping in alpine tundra..
-  # Data set creator:  Jim Halfpenny -  
-  # Contact:    - Information Manager LTER Network Office  - tech-support@lternet.edu
-  # Contact:    - Information Manager Niwot Ridge LTER/University of Colorado  - lternwt@colorado.edu
-  # Metadata Link: https://portal.lternet.edu/nis/metadataviewer?packageid=knb-lter-nwt.43.3
-  # Stylesheet for metadata conversion into program: John H. Porter, Univ. Virginia, jporter@virginia.edu 
+  niwot_raw <- read.csv(paste0(datapath, '/data/paper/raw/niwot/niwot.csv'), stringsAsFactors = F)
   
-  infile1  <- paste0(datapath, '/niwot.csv') 
-  dt1 <-read.csv(infile1,header=F 
-                 ,skip=0
-                 ,sep=","  
-                 ,quot='"' 
-                 , col.names=c(
-                   "collector",     
-                   "date",     
-                   "sex",     
-                   "species",     
-                   "animal.identification.number",     
-                   "weight",     
-                   "age.colon..adult",     
-                   "age.colon..subadult",     
-                   "age.colon..juvenile",     
-                   "male.testes.position.colon..abdominal",     
-                   "male.testes.position.colon..inguinal",     
-                   "male.testes.position.colon..scrotal",     
-                   "imperforate.colon..female.vaginal.condition",     
-                   "perforate.colon..female.vaginal.condition",     
-                   "plug.colon..female.vaginal.condition",     
-                   "small.colon..female.nipple.size",     
-                   "medium.colon..female.nipple.size",     
-                   "large.colon..female.nipple.size",     
-                   "female.lactating.condition",     
-                   "female.pregnancy.condition",     
-                   "condition.of.animal",     
-                   "recapture.status",     
-                   "grid",     
-                   "trap.number",     
-                   "habitat"    ), check.names=TRUE)
-  
-  
-  # Fix any interval or ratio columns mistakenly read in as nominal and nominal columns read as numeric or dates read as strings
-  
-  if (class(dt1$collector)!="factor") dt1$collector<- as.factor(dt1$collector)                                   
-  # attempting to convert dt1$date dateTime string to R date structure (date or POSIXct)                                
-  tmpDateFormat<-"%Y-%m-%d"
-  tmp1date<-as.Date(dt1$date,format=tmpDateFormat)
-  # Keep the new dates only if they all converted correctly
-  # if(length(tmp1date) == length(tmp1date[!is.na(tmp1date)])){dt1$date <- tmp1date } else {print("Date conversion failed for dt1$date. Please inspect the data and do the date conversion yourself.")}                                                         
-  # Dates for row 1599 and 3741 are missing, so use tmp1date anyway
-  dt1$date <- tmp1date 
-  
-  rm(tmpDateFormat,tmp1date) 
-  
-  if (class(dt1$sex)!="factor") dt1$sex<- as.factor(dt1$sex)
-  if (class(dt1$species)!="factor") dt1$species<- as.factor(dt1$species)
-  if (class(dt1$animal.identification.number)!="factor") dt1$animal.identification.number<- as.factor(dt1$animal.identification.number)
-  if (class(dt1$weight)=="factor") dt1$weight <-as.numeric(levels(dt1$weight))[as.integer(dt1$weight) ]
-  if (class(dt1$age.colon..adult)=="factor") dt1$age.colon..adult <-as.numeric(levels(dt1$age.colon..adult))[as.integer(dt1$age.colon..adult) ]
-  if (class(dt1$age.colon..subadult)=="factor") dt1$age.colon..subadult <-as.numeric(levels(dt1$age.colon..subadult))[as.integer(dt1$age.colon..subadult) ]
-  if (class(dt1$age.colon..juvenile)=="factor") dt1$age.colon..juvenile <-as.numeric(levels(dt1$age.colon..juvenile))[as.integer(dt1$age.colon..juvenile) ]
-  if (class(dt1$male.testes.position.colon..abdominal)!="factor") dt1$male.testes.position.colon..abdominal<- as.factor(dt1$male.testes.position.colon..abdominal)
-  if (class(dt1$male.testes.position.colon..inguinal)!="factor") dt1$male.testes.position.colon..inguinal<- as.factor(dt1$male.testes.position.colon..inguinal)
-  if (class(dt1$male.testes.position.colon..scrotal)!="factor") dt1$male.testes.position.colon..scrotal<- as.factor(dt1$male.testes.position.colon..scrotal)
-  if (class(dt1$imperforate.colon..female.vaginal.condition)!="factor") dt1$imperforate.colon..female.vaginal.condition<- as.factor(dt1$imperforate.colon..female.vaginal.condition)
-  if (class(dt1$perforate.colon..female.vaginal.condition)!="factor") dt1$perforate.colon..female.vaginal.condition<- as.factor(dt1$perforate.colon..female.vaginal.condition)
-  if (class(dt1$plug.colon..female.vaginal.condition)!="factor") dt1$plug.colon..female.vaginal.condition<- as.factor(dt1$plug.colon..female.vaginal.condition)
-  if (class(dt1$small.colon..female.nipple.size)!="factor") dt1$small.colon..female.nipple.size<- as.factor(dt1$small.colon..female.nipple.size)
-  if (class(dt1$medium.colon..female.nipple.size)!="factor") dt1$medium.colon..female.nipple.size<- as.factor(dt1$medium.colon..female.nipple.size)
-  if (class(dt1$large.colon..female.nipple.size)!="factor") dt1$large.colon..female.nipple.size<- as.factor(dt1$large.colon..female.nipple.size)
-  if (class(dt1$female.lactating.condition)!="factor") dt1$female.lactating.condition<- as.factor(dt1$female.lactating.condition)
-  if (class(dt1$female.pregnancy.condition)!="factor") dt1$female.pregnancy.condition<- as.factor(dt1$female.pregnancy.condition)
-  if (class(dt1$condition.of.animal)!="factor") dt1$condition.of.animal<- as.factor(dt1$condition.of.animal)
-  if (class(dt1$recapture.status)!="factor") dt1$recapture.status<- as.factor(dt1$recapture.status)
-  if (class(dt1$grid)!="factor") dt1$grid<- as.factor(dt1$grid)
-  if (class(dt1$trap.number)!="factor") dt1$trap.number<- as.factor(dt1$trap.number)
-  if (class(dt1$habitat)!="factor") dt1$habitat<- as.factor(dt1$habitat)
-  
-  # MODIFICATIONS FROM LTER CODE #
-  
-  niwot <- dt1 %>% 
+  niwot <- niwot_raw %>% 
     filter(!is.na(weight)) %>%
     mutate(species = as.character(species)) %>%
     filter(species != "?MOUSE") %>%
     filter(species != "?PIKAS") %>%
     filter(species != "CHIPSP") %>%
-    filter(species != "NaN") %>%
     filter(species != "VOLESP") %>%
-    select(collector, date, species, weight, condition.of.animal, habitat)
+    select(collector, date, species, weight, condition, habitat)
   
   species_means <- niwot %>%
     select(species, weight) %>%
     group_by(species) %>%
-    summarize(mean.weight = mean(weight)) %>%
+    summarize(mean.weight = mean(weight), n.captures = n()) %>%
     ungroup()
   
   big_species <- species_means %>%
@@ -178,6 +100,23 @@ process_niwot_data <- function(datapath = here::here('data', 'paper', 'raw', 'ni
   niwot <- niwot %>%
     filter(!(species %in% big_species$species))
   
+  # Still 1 too many species, based on paper.
+  # Removing pocket gophers because they were only trapped for the first two years? 
+  niwot <- niwot %>%
+    filter(species != 'THOTAL')
+  
+  niwot <- niwot %>% 
+    select(species, weight)
+  
+  processedpath = paste0(datapath, '/data/paper/processed/niwot')
+  if(!dir.exists(processedpath)) {
+    dir.create(processedpath)
+  }
+  
+  write.csv(niwot, paste0(processedpath, '/niwot_processed.csv'))
+ 
+  return(TRUE)
+   
 }
 
 
@@ -192,7 +131,7 @@ process_niwot_data <- function(datapath = here::here('data', 'paper', 'raw', 'ni
 #'
 process_sev_data <- function(datapath = here::here('data', 'paper', 'raw', 'sev')){
   # FROM LTER WEBSITE #
-    # Package ID: knb-lter-sev.8.297976 Cataloging System:https://pasta.lternet.edu.
+  # Package ID: knb-lter-sev.8.297976 Cataloging System:https://pasta.lternet.edu.
   # Data set title: Small Mammal Mark-Recapture Population Dynamics at Core Research Sites at the Sevilleta National Wildlife Refuge, New Mexico (1989 - present).
   # Data set creator:  Seth Newsome - University of New Mexico 
   # Metadata Provider:  Information Manager Sevilleta LTER -  
@@ -238,5 +177,5 @@ process_sev_data <- function(datapath = here::here('data', 'paper', 'raw', 'sev'
                    na=c( "na", ".", " ","NA")  ) 
   # Observed issues when reading the data. An empty list is good!
   problems(dt1) 
- 
+  
 }
